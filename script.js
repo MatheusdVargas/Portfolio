9704168ff1249f7459a356d79a53efd03c0b6aa3
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2024-07-15 20:14:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
